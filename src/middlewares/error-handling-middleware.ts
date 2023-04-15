19d7581788e4572db6de5cf561d93740f8583476
FelipeReis1 <feliperg15@yahoo.com.br>
2023-04-14 22:11:59 -0300

    feat: added // eslint-disable-next-line before _next

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -6,6 +6,7 @@ export function handleApplicationErrors(
   err: ApplicationError | Error,
   _req: Request,
   res: Response,
+  // eslint-disable-next-line
   _next: NextFunction,
 ) {
   if (err.name === 'CannotEnrollBeforeStartDateError') {
