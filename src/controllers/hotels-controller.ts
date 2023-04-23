a4859d5139cb3e206d345ea198bb5df56453b7ba
FelipeReis1 <feliperg15@yahoo.com.br>
2023-04-23 20:47:11 -0300

    fix: error code

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,14 +1,14 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import hotelsService from '@/services/hotels-service';
 
-export async function getHotels(req: AuthenticatedRequest, res: Response) {
+export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const userId = req.userId as number;
   try {
-    const userId = req.userId as number;
     const hotels = await hotelsService.getHotels(userId);
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
-    return res.sendStatus(httpStatus.NOT_FOUND);
+    next(error);
   }
 }
