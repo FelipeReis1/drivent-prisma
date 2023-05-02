02bd0df54f0c93e377e416fb593e1ea9ca3da261
FelipeReis1 <feliperg15@yahoo.com.br>
2023-05-02 02:16:16 -0300

    refact: getBooking

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,17 +1,9 @@
-import { notFoundError, paymentError, forbiddenError } from '@/errors';
+import { notFoundError, forbiddenError } from '@/errors';
 import ticketsRepository from '@/repositories/tickets-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 import bookingsRepository from '@/repositories/bookings-repository';
 
 export async function getBooking(userId: number) {
-  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
-  if (!enrollment) throw notFoundError();
-
-  const ticket = await ticketsRepository.getTicket(userId);
-  if (!ticket) throw notFoundError();
-
-  if (ticket.status !== 'PAID' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) throw paymentError();
-
   const booking = await bookingsRepository.getBooking(userId);
   if (!booking) {
     throw notFoundError();
